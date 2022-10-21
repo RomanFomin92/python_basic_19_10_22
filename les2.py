0ece7874b71855a7d5406bb7da598956e9b6525b
RomanFomin92 <fominroman92@gmail.com>
2022-10-21 19:54:19 +0400

    HW les2

---
+++ les2.py
@@ -0,0 +1,19 @@
+# PEP8
+
+var_str = "Hello world"
+
+i = 0
+# while i < len(var_str):
+#     if i != 5:
+#         print(var_str[i])
+#     i += 1
+# else:
+#     print("ELSE PRINT")
+
+var_list = [1, 2, 3, 'l', 'hello', True]
+
+for itm in var_list:
+    if itm == "l":
+        continue
+    print(itm)
+
