f6729a524cad9191a3c78e18b6d73043d30c5eb0
RomanFomin92 <fominroman92@gmail.com>
2022-10-21 16:09:45 +0400

    HW les 1 task1-task6

--- homeworks/les1/task1.py
+++ homeworks/les1/task1.py
@@ -1,1 +1,10 @@
-"HOME WORK"
+# Практическое задание 1
+
+a = 10
+b = 15
+print("Переменные a и b - ", a, b)
+string1 = input("Введите первую строку ")
+number1 = int(input("Введите первое число "))
+string2 = input("Введите вторую строку ")
+number2 = int(input("Введите второе число "))
+print("Введеные значения - %10s %5d %10s %5d" % (string1, number1, string2, number2))
---
+++ homeworks/les1/task4.py
@@ -0,0 +1,10 @@
+# Практиеское задание 4
+
+n = abs(int(input("Введите число:\n")))
+max = n % 10
+n = n // 10
+while n > 0:
+    if n % 10 > max:
+        max = a % 10
+    n = n // 10
+print(max)
