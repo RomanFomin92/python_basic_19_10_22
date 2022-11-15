3cb9789bb4983d13dca1b6f6a57225e034431c1e
RomanFomin92 <fominroman92@gmail.com>
2022-11-15 15:18:15 +0300

    Task6

--- homeworks/les2/task3.py
+++ homeworks/les2/task3.py
@@ -17,8 +17,8 @@
 month_dict = {'Зима': (1, 2, 12), 'Весна': (3, 4, 5), 'Лето': (6, 7, 8), 'Осень': (9, 10, 11)}
 user_month = int(input('Введите порядковый номер месяца в году (от 1 до 12): '))
 if user_month in range(1, 13):
-    for key in month_dict.keys():
-        if user_month in month_dict[key]:
-            print(f'Введенный номер месяца относится к сезону {key}')
+    for k in month_dict.keys():
+        if user_month in month_dict[k]:
+            print(f'Введенный номер месяца относится к сезону {k}')
 else:
     print('Введен некорректный номер месяца!')
